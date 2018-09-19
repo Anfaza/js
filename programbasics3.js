2e224fa558c73431e6a54887e1e911232522f809
Anfaza <a.frayhi@gmail.com>
2018-09-19 12:28:14 +0200

    first commit

---
+++ programbasics3.js
@@ -0,0 +1,25 @@
+//opdracht 1
+
+var x = 12;
+
+var y = x/2;
+var w = Math.floor(x/2);
+
+if (  y==w ) {
+    console.log ('even'+ x);
+
+
+}
+//opdracht 2
+else {
+     console.log ('uneven'+ x)
+    }
+
+    const coolString = 'Programming is not so cool'
+    console.log (coolString);
+    const coolReplacedString = coolString.replace ( 'not','');
+    console.log(coolReplacedString);
+
+    //opdracht 3 het is geen handige vergelijking omdat de een een getal is en de ander een zin.
+
+console.log (1400==' Ik woon in Naboo');
